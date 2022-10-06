89d6f38a7a846ccdb82051a614b35e62e94b3c04
unaldutlu <unaldutlu@gmail.com>
2022-10-06 18:23:40 +0300

    jsx

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
+import React from "react";
 import "./App.css";
 import Header from "./components/headers/Header.jsx";
 
 function App() {
   return (
-    <div>
+    // <div>
+    <React.Fragment>
       <h1>Hello React</h1>
       <Header />
-    </div>
+      <p className='jsx'>Lorem ipsum</p>
+      <label htmlFor='myinput'>Name</label>
+      <input id='myinput'></input>
+    </React.Fragment>
+    // </div>
   );
 }
 
