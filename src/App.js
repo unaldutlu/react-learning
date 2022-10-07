f99feee5735ed74bf02198a1041d32cd19f89554
unaldutlu <unaldutlu@gmail.com>
2022-10-07 16:21:50 +0300

    Ternary Operator

--- src/App.js
+++ src/App.js
@@ -1,18 +1,37 @@
-import React from "react";
+// import React from "react";
+// import Header from "./components/headers/Header.jsx";
 import "./App.css";
-import Header from "./components/headers/Header.jsx";
+
+const name = "Unal";
+const surname = "Dutlu";
+const isLoggedIn = true;
 
 function App() {
   return (
-    // <div>
-    <React.Fragment>
-      <h1>Hello React</h1>
-      <Header />
-      <p className='jsx'>Lorem ipsum</p>
-      <label htmlFor='myinput'>Name</label>
-      <input id='myinput'></input>
-    </React.Fragment>
-    // </div>
+    // <React.Fragment>
+    //   <h1>Hello React</h1>
+    //   <Header />
+    //   <p className='jsx'>Lorem ipsum</p>
+    //   <label htmlFor='myinput'>Name</label>
+    //   <input id='myinput'></input>
+    // </React.Fragment>
+    // ! *******---------------**************
+    // <>
+    //   <h1>{name}</h1>
+    //   <h1>{surname}</h1>
+    //   <h2>{`benim adım ${name}, soyadım ${surname}`}</h2>
+    // </>
+    // ! *************-----------************
+    <>
+      {/* <h1>{isLoggedIn && `benim adım ${name}, soyadım ${surname}`}</h1>
+      <h1>{!isLoggedIn && `Giriş yapmadınız...`}</h1> */}
+
+      <h1>
+        {isLoggedIn
+          ? `benim adım ${name}, soyadım ${surname}`
+          : `Giriş yapmadınız...`}
+      </h1>
+    </>
   );
 }
 
