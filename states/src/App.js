c75b430e9183a8cff457a39f8537decca482e2c0
unaldutlu <unaldutlu@gmail.com>
2022-10-10 04:01:37 +0300

    array states

--- states/src/App.js
+++ states/src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [name, setName] = useState("Dutlu");
+  const [age, setAge] = useState(29);
+  const [friends, setFriends] = useState(["Unal", "Dutlu"]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Merhaba {name}!</h1>
+      <h2>{age}</h2>
+      <button onClick={() => setName("Unal")}>Name</button>
+      <button onClick={() => setAge(30)}>Age</button>
+      <hr />
+      <h2>Friends</h2>
+      {friends.map((friends, index) => (
+        <div key={index}>{friends}</div>
+      ))}
+      <br />
+      <button onClick={() => setFriends([...friends, "Mutlu"])}>
+        add new friend
+      </button>
     </div>
   );
 }
