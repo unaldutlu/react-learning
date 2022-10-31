3db263de5e2d584f7db84cf9d28df427c650106f
unaldutlu <unaldutlu@gmail.com>
2022-10-31 11:58:31 +0300

    add formik on page

--- formik/src/App.js
+++ formik/src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Formik } from "formik";
+import Formik1 from "./components/Formik1";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>Sign Up</h1>
+      <Formik
+        initialValues={{
+          firstName: "",
+          lastName: "",
+          email: "",
+        }}
+        onSubmit={async (values) => {
+          await new Promise((r) => setTimeout(r, 500));
+          alert(JSON.stringify(values, null, 2));
+        }}
+      >
+        {({ handleSubmit, handleChange }) => (
+          <form onSubmit={handleSubmit}>
+            <label htmlFor='firstName'>First Name</label>
+            <input
+              id='firstName'
+              name='firstName'
+              placeholder='Jane'
+              onChange={handleChange}
+            />
+            <br />
+
+            <label htmlFor='lastName'>Last Name</label>
+            <input
+              id='lastName'
+              name='lastName'
+              placeholder='Doe'
+              onChange={handleChange}
+            />
+            <br />
+
+            <label htmlFor='email'>Email</label>
+            <input
+              id='email'
+              name='email'
+              placeholder='jane@acme.com'
+              type='email'
+              onChange={handleChange}
+            />
+            <br />
+            <button type='submit'>Submit</button>
+          </form>
+        )}
+      </Formik>
+      <Formik1 />
     </div>
   );
 }
