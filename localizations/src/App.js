276c2e16c6b5c86ce0a6cccedfdad936c9020e40
unaldutlu <unaldutlu@gmail.com>
2022-12-16 22:34:50 +0300

    add localixations update on page

--- localizations/src/App.js
+++ localizations/src/App.js
@@ -1,9 +1,38 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import { IntlProvider, FormattedMessage, useIntl } from "react-intl";
+
+const messages = {
+  "tr-TR": {
+    title: "Merhaba Dünya",
+    description: "{count} yeni mesajiniz var",
+  },
+  "en-US": {
+    title: "Hello World",
+    description: "You have {count} new messages",
+  },
+};
 
 function App() {
+  const isLocale = localStorage.getItem("locale");
+  const defaultLocale = isLocale ? isLocale : navigator.language;
+  const [locale, setLocale] = useState(defaultLocale);
+
+  useEffect(() => {
+    localStorage.setItem("locale", locale);
+  }, [locale]);
+
   return (
-    <div>
-      <h1>Hello</h1>
+    <div className='App'>
+      <IntlProvider locale={locale} messages={messages[locale]}>
+        <FormattedMessage id='title' />
+        <p>
+          <FormattedMessage id='description' values={{ count: 4 }} />
+        </p>
+        <br />
+        <br />
+        <button onClick={() => setLocale("tr-TR")}>TR</button>&nbsp;
+        <button onClick={() => setLocale("en-US")}>EN</button>
+      </IntlProvider>
     </div>
   );
 }
