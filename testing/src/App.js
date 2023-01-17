8e30eb2e726a19f268453945a3fb02d2d8083edc
unaldutlu <unaldutlu@gmail.com>
2023-01-17 23:23:07 +0300

    add todo folder on page

--- testing/src/App.js
+++ testing/src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
-import Counter from "./components/Counter";
+// import Counter from "./components/Counter";
+import Todo from "./components/Todo/Todo";
 
 function App() {
   return (
     <div className='App'>
-      <Counter />
+      {/* <Counter /> */}
+      <Todo />
     </div>
   );
 }
