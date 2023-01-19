47418d68140d6ffc0fb163df7d5a06d3f3f70519
unaldutlu <unaldutlu@gmail.com>
2023-01-19 23:38:27 +0300

    add toast om page

--- testing/src/components/Todo/Todo.jsx
+++ testing/src/components/Todo/Todo.jsx
@@ -1,9 +1,59 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import toast, { Toaster } from "react-hot-toast";
 
 function Todo() {
+  const [form, setForm] = useState();
+  const [todos, setTodos] = useState([]);
+
+  const handleChange = (e) => {
+    setForm(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (form === "") {
+      return false;
+    }
+    setTodos([...todos, form]);
+    setForm("");
+  };
+
+  const notify = () => {
+    if (form !== "") {
+      toast("Tebrikler...", {
+        icon: "👏",
+        style: {
+          borderRadius: "10px",
+          background: "#333",
+          color: "#fff",
+        },
+      });
+    }
+  };
+
   return (
     <div>
       <h1>Todo</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          name='name'
+          value={form}
+          onChange={handleChange}
+          autoComplete='off'
+        />
+        &nbsp;
+        <button onClick={notify}>Add</button>
+        <Toaster />
+      </form>
+      <div>
+        {todos.map((el, i) => (
+          <div key={i}>
+            <ul>
+              <li>{el}</li>
+            </ul>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
